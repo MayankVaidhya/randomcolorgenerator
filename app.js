0820e50fcc0090ec51477e5b71f33efdabcf8af0
MayankVaidhya <vaidhyamayank490@gmail.com>
2023-03-03 12:58:24 +0530

    Random Color Generator

---
+++ app.js
@@ -0,0 +1,15 @@
+const getcolor = () => {
+    const randomnumber = Math.floor(Math.random() * 16777215);
+    const randomCode = '#' + randomnumber.toString(16);
+    document.body.style.background = randomCode;
+    document.getElementById('color_code').innerText = randomCode;
+
+    navigator.clipboard.writeText(randomCode);
+}
+
+document.getElementById('btn').addEventListener(
+    'click',
+    getcolor
+)
+
+getcolor();
